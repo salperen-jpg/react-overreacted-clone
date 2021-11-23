b4bb7ff4097dc04aea7e87237a0fe47f05ee3ec4
Salih Alperen <sal.alperen1@gmail.com>
2021-11-23 14:54:45 +0100

    fixed

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -2,12 +2,15 @@ import React, { useState, useEffect } from 'react';
 
 const Navbar = () => {
   const [theme, setTheme] = useState('light-theme');
+  const [color, setColor] = useState(false);
 
   const changeTheme = () => {
     if (theme === 'light-theme') {
       setTheme('dark-theme');
+      setColor(true);
     } else {
       setTheme('light-theme');
+      setColor(false);
     }
   };
   useEffect(() => {
@@ -17,7 +20,12 @@ const Navbar = () => {
     <nav>
       <div className='nav-center'>
         <h1 className='logo'>Overreacted</h1>
-        <button onClick={changeTheme}>Toggle</button>
+        <button onClick={changeTheme} className='btn'>
+          <i
+            class='fas fa-adjust'
+            style={{ color: color ? 'white' : 'black' }}
+          ></i>
+        </button>
       </div>
     </nav>
   );
